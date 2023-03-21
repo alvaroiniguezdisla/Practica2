ceea2413fa14c6ecd3d4bc47cf448d76e4f4eced
Álvaro <ainiguezdislaa@gmail.com>
2023-03-21 09:57:43 +0100

    practica 2 terminada

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,45 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+import dominio.Tablero;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.lang.InterruptedException;
+public class Principal{
+    public static void main(String[] args){
+        Tablero tablero = new Tablero();
+        try {
+
+            System.out.println("SIMULACIÓN CON TABLERO LEÍDO ");
+            tablero.leerEstadoActual();
+            System.out.println(tablero);
+            for (int i = 0; i <= 5; i++) {
+                TimeUnit.SECONDS.sleep(1);
+                tablero.transitarAlEstadoSiguiente();
+                System.out.println(tablero);
+                System.out.println("Siguiente tablero");
+            }
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }catch(IOException e) {
+            System.out.println(e);
+        }catch (InterruptedException e) {
+            System.out.println(e);
+        }try{
+            System.out.println("SIMULACIÓN CON TABLERO GENERADO MEDIANTE MONTECARLO");
+            tablero.generarEstadoActualPorMontecarlo();
+            System.out.println(tablero);
+            for(int i = 0; i <= 15; i++)
+            {
+                TimeUnit.SECONDS.sleep(1);
+                tablero.transitarAlEstadoSiguiente();
+                System.out.println(tablero);
+            }
+        }catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+
+    }
 }
--- src/dominio/Tablero.java
+++ src/dominio/Tablero.java
@@ -1,2 +1,94 @@
-package dominio;public class Tablero {
+package dominio;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Tablero {
+    private static int DIMENSION = 30;
+    private int estadoActual[][] = new int[DIMENSION + 2][DIMENSION + 2];
+    private int estadoSiguiente[][] = new int[DIMENSION + 2][DIMENSION + 2];
+
+    public void leerEstadoActual() throws FileNotFoundException, IOException {
+        FileReader fr = new FileReader("matriz.txt.txt");
+
+            for (int i = 0; i <= DIMENSION + 1; i++) {
+                for (int j = 0; j <= DIMENSION + 1; j++) {
+                    estadoActual[i][j] = 0;
+            }
+        }
+
+             for (int i = 1; i <= DIMENSION; i++) {
+
+                 for (int j = 1; j <= DIMENSION; j++) {
+                 estadoActual[i][j] = fr.read()== 49?1:0;
+             }
+
+             fr.read();
+             fr.read();
+         }
+
+
+
+    }
+
+    public void generarEstadoActualPorMontecarlo() {
+        double numero;
+        for (int i = 1; i <= DIMENSION; i++) {
+            for (int j = 1; j <= DIMENSION; j++) {
+                numero =  (Math.random() );
+                if (numero < 0.7) {
+                    estadoActual[i][j] = 0;
+                } else {
+                    estadoActual[i][j] = 1;
+                }
+
+            }
+        }
+    }
+
+    public void transitarAlEstadoSiguiente() {
+        int vecinos;
+        for (int i = 1; i <= DIMENSION ; i++) {
+            for (int j = 1; j <= DIMENSION ; j++) {
+                vecinos = estadoActual[i - 1][j - 1] + estadoActual[i - 1][j] + estadoActual[i - 1][j + 1] + estadoActual[i][j - 1] + estadoActual[i][j + 1] + estadoActual[i + 1][j - 1] + estadoActual[i + 1][j] + estadoActual[i + 1][j + 1];
+                if (estadoActual[i][j] == 1) {
+                    if (vecinos == 2 || vecinos == 3) {
+                        estadoSiguiente[i][j] = 1;
+                    } else {
+                        estadoSiguiente[i][j] = 0;
+                    }
+
+
+                } else if (estadoActual[i][j] == 0) {
+                    if (vecinos == 3) {
+                        estadoSiguiente[i][j] = 1;
+                    } else {
+                        estadoSiguiente[i][j] = 0;
+                    }
+                }
+            }
+
+
+        }
+        estadoActual = estadoSiguiente;
+    }
+
+    public String toString() {
+        String cadena = "";
+        for (int i = 1; i <= DIMENSION; i++) {
+            for (int j = 1; j <= DIMENSION; j++) {
+                if (estadoActual[i][j] == 1) {
+                    cadena += "1";
+                } else {
+                    cadena += "0";              }
+            }
+            cadena += "\n";
+
+        }
+        return cadena;
+    }
+
+
 }
